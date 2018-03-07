715424dc40036ed9edf9bfd2266f3896a6a0722c
Ahmed Hosni <ahmed.hosni1988@gmail.com>
2018-03-07 04:20:36 +0200

    all

--- server/server.js
+++ server/server.js
@@ -17,28 +17,40 @@ var io = socketIO(server);
 
 app.use(express.static(publicpath));
 
-io.on('connection',(socket)=>{
+
+io.on('connection', (socket) => {
     console.log('New user connected');
-
-    socket.on('disconnect',()=>{
-        console.log('user disconnected');
+  
+    socket.emit('newMessage', {
+      from: 'Admin',
+      text: 'Welcome to the chat app',
+      createdAt: new Date().getTime()
     });
-
-
-    socket.on('connect',()=>{
-        io.emit('message',message);
+  
+    socket.broadcast.emit('newMessage', {
+      from: 'Admin',
+      text: 'New user joined',
+      createdAt: new Date().getTime()
     });
-    
-    socket.on('send',(message)=>{
-        io.emit('message',message);
-       
+  
+    socket.on('createMessage', (message) => {
+      console.log('createMessage', message);
+      io.emit('newMessage', {
+        from: message.from,
+        text: message.text,
+        createdAt: new Date().getTime()
+      });
+      // socket.broadcast.emit('newMessage', {
+      //   from: message.from,
+      //   text: message.text,
+      //   createdAt: new Date().getTime()
+      // });
     });
-
-
-    
-
-
-});
+  
+    socket.on('disconnect', () => {
+      console.log('User was disconnected');
+    });
+  });
 
 server.listen(port,() =>{
     console.log(`Server is up on port ${port}`);
